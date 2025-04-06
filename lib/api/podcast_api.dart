33a82008a08fe1e23e607b151d8f11dadc393e43
Kennie265 <kennedyphiri2018@gmail.com>
2025-04-07 00:42:54 +0200

    Implement podcast fetching with Dio and internet connection checking

--- lib/api/podcast_api.dart
+++ lib/api/podcast_api.dart
@@ -1,3 +1,28 @@
+import 'package:internet_connection_checker/internet_connection_checker.dart';
+import 'package:dio/dio.dart';
+import '../../model/podcast.dart';
+
 class PodcastApi {
-  Futue <List<Podcast>> fethPodcast() async{}
+  final dio = Dio();
+  Future <List<Podcast>> fethPodcast() async{
+    const url = 'https://listen-api.listennotes.com/api/v2/search?q=podcast&sort_by_date=0&len_min=0&len_max=0&offset=0&safe_mode=0&next_offset=0&previous_offset';
+    const apiKey = '8617058a766048e3a5ebe6f3fd229956';
+    final heders =  {
+      'X-ListeAPI-Key': apiKey,
+      'Accept' : 'application/json',
+    };
+    if (await InternetConnectionChecker().hasConnection) {
+      final response = await dio.get(
+        url,
+        options: Options(
+          headers: heders,
+        ),
+      );
+      final result = response.data['results'] as List;
+      final podcasts = result.map((e) => Podcast.fromJson(e)).toList();
+      return podcasts;
+    } else{
+      return [];
+    }
+  }
 }
